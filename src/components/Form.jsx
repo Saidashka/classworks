f43ff8db1cbfc2103b871d310ed1a40f8ed973cf
Saida Makhmudova <saida66779955@gmail.com>
2021-08-03 19:34:19 +0200

    classworks

---
+++ src/components/Form.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+const Form = ({onAdd}) => {
+    const [todoItem, setTodoItem]= useState('')
+    const changeHandle = event => {
+        setTodoItem(event.target.value)
+    }
+  const onSubmitHandle = event => {
+      event.preventDefault()
+      if (!todoItem.trim().length) return null
+
+    onAdd(todoItem.trim())
+    setTodoItem('')
+  }
+  return (
+  <div className="container pt-5">
+      <div className="w-75 mx-auto border border-1 
+      border-success shadow p-2 rounded-2">
+          <form onSubmit = {
+              onSubmitHandle}>
+                  <div className="input-group">
+                      <input type="text"
+                      className="form-control"
+                      placeholder="Какие у тебя дела на сегодня?"
+                      onChange={changeHandle}
+                      value={todoItem}/>
+                      <button className="btn btn-primary"
+                      type="submit">Add</button>
+                  </div>
+              </form>
+      </div>
+  </div>
+  );
+};
+
+
+export default Form;
